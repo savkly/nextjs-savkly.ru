1e0abf9f3c49370d0ab8ae9c67e104e146e18fda
Saveliy <81488074+savkly@users.noreply.github.com>
2025-09-26 14:16:17 +0300

    upd

--- app/page.js
+++ app/page.js
@@ -1,176 +1,19 @@
 "use client";
 
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import Image from "next/image";
 
 export default function Home() {
-  const [rssItems, setRssItems] = useState([]);
-  const [filterWords, setFilterWords] = useState('');
-  const [filteredItems, setFilteredItems] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [visibleItem, setVisibleItem] = useState(null);
-  const [showMessage, setShowMessage] = useState(false);
-
-  // При монтировании загружаем данные из localStorage
-  useEffect(() => {
-    const savedFilterWords = localStorage.getItem('filterWords');
-    if (savedFilterWords) {
-      setFilterWords(savedFilterWords);
-    }
-  }, []);
-
-  // Сохраняем данные в localStorage при каждом изменении filterWords
-  useEffect(() => {
-    if (filterWords) {
-      localStorage.setItem('filterWords', filterWords);
-    }
-  }, [filterWords]);
-
-  // Загрузка RSS ленты
-  const fetchRssFeed = async () => {
-    try {
-      const response = await axios.get('/api/rss');
-      const parser = new DOMParser();
-      const xml = parser.parseFromString(response.data, 'application/xml');
-
-      const items = Array.from(xml.querySelectorAll('item')).map((item) => ({
-        title: item.querySelector('title').textContent,
-        link: item.querySelector('link').textContent,
-        pubDate: item.querySelector('pubDate').textContent,
-        description: item.querySelector('description')?.textContent || 'Нет описания',
-        category: item.querySelector('category')?.textContent || 'Без категории',
-      }));
-
-      setRssItems(items);
-      setShowMessage(true); // Показать сообщение "обновлено"
-      setTimeout(() => setShowMessage(false), 2000); // Скрыть через 2 секунды
-    } catch (err) {
-      setError('Не удалось загрузить RSS ленту');
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // При монтировании загружаем RSS ленту
-  useEffect(() => {
-    fetchRssFeed();
-  }, []);
-
-  // Фильтрация новостей по ключевым словам
-  useEffect(() => {
-    if (filterWords) {
-      const words = filterWords
-        .split('\n')
-        .map((word) => word.trim().toLowerCase())
-        .filter(Boolean);
-
-      const filtered = rssItems.filter((item) =>
-        words.some((word) =>
-          item.title.toLowerCase().includes(word) ||
-          item.description.toLowerCase().includes(word) ||
-          item.category.toLowerCase().includes(word)
-        )
-      );
-      setFilteredItems(filtered);
-    } else {
-      setFilteredItems(rssItems);
-    }
-  }, [filterWords, rssItems]);
-
-  // Обработчик изменения текста в textarea
-  const handleFilterChange = (event) => {
-    setFilterWords(event.target.value);
-  };
-
-  // Показать/скрыть новость
-  const toggleVisibility = (index) => {
-    setVisibleItem((prevIndex) => (prevIndex === index ? null : index));
-  };
-
-  // Подсветка текста
-  const highlightText = (text, words) => {
-    if (!words.length) return text;
-
-    const regex = new RegExp(`(${words.join('|')})`, 'gi');
-    return text.replace(regex, (match) => `<mark>${match}</mark>`);
-  };
-
-  // Подсветка заголовка
-  const highlightedTitle = (title, words) => {
-    const safeWords = words.map((word) => word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')); // Экранирование спецсимволов
-    return <span dangerouslySetInnerHTML={{ __html: highlightText(title, safeWords) }} />;
-  };
-
-  // Подсветка описания
-  const highlightedDescription = (description, words) => {
-    const safeWords = words.map((word) => word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')); // Экранирование спецсимволов
-    return <span dangerouslySetInnerHTML={{ __html: highlightText(description, safeWords) }} />;
-  }
-
-  // Подсветка категории
-  const highlightedCategory = (category, words) => {
-    const safeWords = words.map((word) => word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')); // Экранирование спецсимволов
-    return <span className="category" dangerouslySetInnerHTML={{ __html: highlightText(category, safeWords) }} />;
-  }
-
-  if (loading) return <p>Загрузка...</p>;
-  if (error) return <p>Ошибка: {error}</p>;
-
-  // Преобразование строки с ключевыми словами в массив
-  const filterWordsArray = filterWords
-    .split('\n')
-    .map((word) => word.trim().toLowerCase())
-    .filter(Boolean);
 
   return (
-    <div className="page">
-      {showMessage && <div className="update-message fade-in">Обновлено</div>}
-      <h1 className="mb-20">Фильтр новостей MK.ru по ключевым словам</h1>
-
-      <textarea
-        placeholder="Введите ключевые слова (одно на строке)"
-        value={filterWords}
-        onChange={handleFilterChange}
-        rows={5}
-        style={{ width: '100%', marginBottom: '20px' }}
-      />
-
-      <div className="mb-20" style={{ display: 'flex', gap: '10px' }}>
-        <button className="btn" onClick={() => setFilterWords('')}>Сбросить фильтр</button>
-        <button className="btn btn-primary" onClick={() => fetchRssFeed()}>Обновить</button>
+    <div className="grid place-content-center h-full">
+      <div className="flex md:flex-col flex-row items-center">
+        <Image src='./img/savkly.png' width={310} height={354} alt="SavKly" />
+        <div className="border p-2 rounded-md">
+          <p>Привет, данный сайт находиться на стадии разработки...</p>
+          <a href="https://savkly.ru">Основной сайт</a><br />
+          <a href="https://portfolio.savkly.ru">Открыть моё портфолио</a>
+        </div>
       </div>
-
-      <h2>Новости:</h2>
-
-      <ul>
-        {filteredItems.length > 0 ? (
-          filteredItems.map((item, index) => (
-            <li key={index}>
-              <div className={`newsItem ${visibleItem === index ? 'selected' : ''}`}>
-                <h3
-                  onClick={() => toggleVisibility(index)}
-                  style={{ cursor: 'pointer', marginBottom: '10px' }}
-                >
-                  {highlightedTitle(item.title, filterWordsArray)}
-                </h3>
-                <div>
-                  Категория: <span className="category">{highlightedCategory(item.category, filterWordsArray)}</span>
-                  <a href={item.link} className="btn btn-sm" target="_blank" rel="noopener noreferrer">🌐 Открыть ссылку</a>
-                </div>
-                {visibleItem === index && (
-                  <div>
-                    <p>{highlightedDescription(item.description, filterWordsArray)}</p>
-                    <small>{new Date(item.pubDate).toLocaleString()}</small>
-                  </div>
-                )}
-              </div>
-            </li>
-          ))
-        ) : (
-          <p>Нет новостей по заданным ключевым словам.</p>
-        )}
-      </ul>
     </div>
   );
 }
